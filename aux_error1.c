2897933a249212eeef40d04d2ef8701fe9524ebc
Nyareki Gospel <nyarekigospel@gmail.com>
2023-02-22 08:17:08 +0300

    Update

--- aux_error1.c
+++ aux_error1.c
@@ -3,9 +3,9 @@
 /**
  *  * strcat_cd - function that concatenates the message for cd error
  *   *
- *    * @datash: data relevant (directory)
- *     * @msg: message to print
- *      * @error: output message
+ * @datash: data relevant (directory)
+ * @msg: message to print
+ * @error: output message
  *       * @ver_str: counter lines
  *        * Return: error message
  *         */
